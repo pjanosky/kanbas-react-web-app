0a6f09c5cefc6133194dec0768f7046a083deae7
pjanosky <pcjanosky@gmail.com>
2024-01-13 18:48:21 -0500

    assignment 1 lab

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from "react";
+/*
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -17,3 +19,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/

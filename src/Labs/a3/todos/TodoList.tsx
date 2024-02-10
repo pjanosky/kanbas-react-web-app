f27f9e956fc7eb0e5517cc113ce21524184201bd
pjanosky <pcjanosky@gmail.com>
2024-02-10 14:57:36 -0500

    impelment lab 3

---
+++ src/Labs/a3/todos/TodoList.tsx
@@ -0,0 +1,15 @@
+import TodoItem from "./TodoItem";
+import todos from "./todos.json";
+const TodoList = () => {
+  return (
+    <>
+      <h3>Todo List</h3>
+      <ul className="list-group">
+        {todos.map((todo, index) => {
+          return <TodoItem todo={todo} key={index} />;
+        })}
+      </ul>
+    </>
+  );
+};
+export default TodoList;
